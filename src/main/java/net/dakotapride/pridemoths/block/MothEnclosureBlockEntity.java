b66f5f0dcc4a2e7908d87aad2146b691ec8cabba
Dakota <rjtube13@gmail.com>
2025-05-02 18:47:44 -0500

    .

--- src/main/java/net/dakotapride/pridemoths/block/MothEnclosureBlockEntity.java
+++ src/main/java/net/dakotapride/pridemoths/block/MothEnclosureBlockEntity.java
@@ -212,7 +212,7 @@ public class MothEnclosureBlockEntity extends BlockEntity implements Inventory, 
 
     @Override
     public Text getDisplayName() {
-        return this.getName();
+        return Text.of("Moth Enclosure");
     }
 
     @Nullable
