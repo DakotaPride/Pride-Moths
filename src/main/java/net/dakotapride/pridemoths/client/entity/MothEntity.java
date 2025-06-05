3c9c18ef49c7660096956dc831aa72b8aa194a2d
Dakota <rjtube13@gmail.com>
2025-06-05 11:30:54 -0500

    Fix variations issue

--- src/main/java/net/dakotapride/pridemoths/client/entity/MothEntity.java
+++ src/main/java/net/dakotapride/pridemoths/client/entity/MothEntity.java
@@ -108,18 +108,18 @@ public class MothEntity extends AnimalEntity implements GeoEntity, Flutterer, IP
             MothVariation.AROMANTIC, MothVariation.DEMIROMANTIC, MothVariation.AROACE);
 
     public static MothVariation getAceVariation(Random random) {
-        return PRIDE_VARIATIONS.get(random.nextInt(ASEXUAL_VISIBILITY_DAY_VARIATIONS.size()));
+        return ASEXUAL_VISIBILITY_DAY_VARIATIONS.get(random.nextInt(ASEXUAL_VISIBILITY_DAY_VARIATIONS.size()));
     }
 
     public static MothVariation getAroVariation(Random random) {
-        return PRIDE_VARIATIONS.get(random.nextInt(AROMANTIC_VISIBILITY_DAY_VARIATIONS.size()));
+        return AROMANTIC_VISIBILITY_DAY_VARIATIONS.get(random.nextInt(AROMANTIC_VISIBILITY_DAY_VARIATIONS.size()));
     }
 
     public static final List<MothVariation> DEMIGENDER_VISIBILITY_DAY_VARIATIONS = List.of(
             MothVariation.DEMIBOY, MothVariation.DEMIGIRL, MothVariation.DEMIGENDER);
 
     public static MothVariation getDemigenderVariation(Random random) {
-        return PRIDE_VARIATIONS.get(random.nextInt(DEMIGENDER_VISIBILITY_DAY_VARIATIONS.size()));
+        return DEMIGENDER_VISIBILITY_DAY_VARIATIONS.get(random.nextInt(DEMIGENDER_VISIBILITY_DAY_VARIATIONS.size()));
     }
 
     public static final List<MothVariation> TRANSGENDER_VISIBILITY_DAY_VARIATIONS = List.of(
@@ -129,7 +129,7 @@ public class MothEntity extends AnimalEntity implements GeoEntity, Flutterer, IP
             MothVariation.BIGENDER, MothVariation.PANGENDER);
 
     public static MothVariation getTransgenderVariation(Random random) {
-        return PRIDE_VARIATIONS.get(random.nextInt(TRANSGENDER_VISIBILITY_DAY_VARIATIONS.size()));
+        return TRANSGENDER_VISIBILITY_DAY_VARIATIONS.get(random.nextInt(TRANSGENDER_VISIBILITY_DAY_VARIATIONS.size()));
     }
 
     public static MothVariation getOtherVariation(Random random) {
